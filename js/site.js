2880f450498e443c594c66119e68bce1834fb01d
Yasitha Silva <97441964+yasithasilva@users.noreply.github.com>
2024-04-11 09:14:07 +0530

    Adding File Structure

---
+++ js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.onload = function(){
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    
+    closeBtn.addEventListener("click",function(){
+        sidebar.classList.toggle("open")
+    })
+
+    function menuBtnChange(){
+        if(sidebar.classList.contains("open")){
+            closeBtn.classList.replace("fas fa-bars","fas fa-bars")
+        }
+
+        else{
+            closeBtn.classList.replace("fas fa-bars","fas fa-bars")
+        }
+    }
+}
+
